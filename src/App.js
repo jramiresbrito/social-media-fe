edc56d1011541308aed9809183c2a5001782302b
João Victor Ramires Guimarães Brito <joaoramires.engsoft@gmail.com>
2020-01-17 19:28:24 -0300

    Feature: ADD post form

--- src/App.js
+++ src/App.js
@@ -1,10 +1,20 @@
-import React from 'react';
+import React, { Component } from 'react';
+import { Route, Redirect, Switch } from 'react-router-dom';
+import { ToastContainer } from 'react-toastify';
+import Posts from './components/posts';
+import NotFound from './components/notFound';
+
 import Posts from './components/posts';
 
-function App() {
-  return (
-    <Posts />
-  );
+class App extends Component {
+  state = {  }
+  render() {
+    return (
+      <>
+
+      </>
+    );
+  }
 }
 
 export default App;
---
+++ src/components/postForm.jsx
@@ -0,0 +1,92 @@
+import React from 'react';
+import Joi from 'joi-browser';
+import Form from './common/form';
+import { getPosts, savePost } from '../services/postService';
+import { getNetworks } from '../services/networkService';
+
+class PostForm extends Form {
+  state = {
+    data: {
+      Author: '',
+      networkId: '',
+      date: '',
+      link: '',
+    },
+    networks: [],
+    errors: {},
+  };
+
+  schema = {
+    _id: Joi.string(),
+    author: Joi.string()
+      .required()
+      .label('Author'),
+    networkId: Joi.string()
+      .required()
+      .label('Network'),
+    post_date: Joi.date()
+      .required()
+      .label('Post Date'),
+    post_link: Joi.string()
+      .required()
+      .min(5)
+      .max(300)
+      .label('Post Link'),
+  };
+
+  async populateNetworks() {
+    const { data: networks } = await getNetworks();
+    this.setState({ networks });
+  }
+
+  async populatePost() {
+    try {
+      const postId = this.props.match.params.id;
+      if (postId === 'new') return;
+
+      const { data: post } = await getPosts(postId);
+      this.setState({ data: this.mapToViewModel(post) });
+    } catch (error) {
+      if (error.response && error.response.status === 404)
+        this.props.history.replace('/not-found');
+    }
+  }
+
+  async componentDidMount() {
+    await this.populateNetworks();
+    await this.populatePost();
+  }
+
+  mapToViewModel(post) {
+    return {
+      _id: post._id,
+      author: post.author.name,
+      networkId: post.network._id,
+      post_date: post.post_date,
+      post_link: post.post_link,
+    };
+  }
+
+  doSubmit = async () => {
+    await savePost(this.state.data);
+
+    this.props.history.push('/post');
+  };
+
+  render() {
+    return (
+      <div className='col-5'>
+        <h1>Post Form</h1>
+        <form onSubmit={this.handleSubmit}>
+          {this.renderInput('author', 'Author')}
+          {this.renderSelect('networkId', 'Network', this.state.networks)}
+          {this.renderInput('post_date', 'Post Date', 'date')}
+          {this.renderInput('post_link', 'Post Link')}
+          {this.renderButton('Save')}
+        </form>
+      </div>
+    );
+  }
+}
+
+export default PostForm;
