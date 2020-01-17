3fec957d83edfa4e6eafaf2e6d86dac9f7f6a7be
João Victor Ramires Guimarães Brito <joaoramires.engsoft@gmail.com>
2020-01-17 00:28:10 -0300

    Feature: Call backend services

--- src/components/posts.jsx
+++ src/components/posts.jsx
@@ -1,32 +1,43 @@
 import React, { Component } from 'react';
+import { getPosts } from '../services/postService';
+import { getNetworks } from '../services/networkService';
 
 class Posts extends Component {
   state = {
     posts: [],
     networks: [],
-   }
+  }
+
+  async componentDidMount() {
+    const {data} = await getNetworks();
+    const networks = [{ _id: '', name: 'All Networks'}, ...data];
+
+    const {data: posts} = await getPosts();
+    this.setState({posts, networks});
+  }
+
   render() {
     return (
-    <table className='table table-hover table-striped'>
-      <thead>
-        <tr>
-          <th>Author</th>
-          <th>Date</th>
-          <th>Network</th>
-          <th>Lists</th>
-          <th>Link</th>
-        </tr>
-      </thead>
-      <tbody>
-        <tr>
-          <td><a href="#">Author Name</a></td>
-          <td>Post Date</td>
-          <td>Post Network</td>
-          <td>People Lists</td>
-          <td><a href="#">Link</a></td>
-        </tr>
-      </tbody>
-    </table>
+      <table className='table table-hover table-striped'>
+        <thead>
+          <tr>
+            <th>Author</th>
+            <th>Date</th>
+            <th>Network</th>
+            <th>Lists</th>
+            <th>Link</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td><a href="#">Author Name</a></td>
+            <td>Post Date</td>
+            <td>Post Network</td>
+            <td>People Lists</td>
+            <td><a href="#">Link</a></td>
+          </tr>
+        </tbody>
+      </table>
     );
   }
 }
--- src/services/httpService.js
+++ src/services/httpService.js
@@ -1,11 +1,14 @@
 import axios from 'axios';
-import {toast} from 'react-toastify';
+import { toast } from 'react-toastify';
 import logger from './logService';
 
-axios.defaults.baseURL = process.env.SOCIAL_APP_API_URL;
+axios.defaults.baseURL = process.env.REACT_APP_API_URL;
 
 axios.interceptors.response.use(null, error => {
-  const expectedError = error.response && error.response.status >= 400 && error.response.status < 500;
+  const expectedError =
+    error.response &&
+    error.response.status >= 400 &&
+    error.response.status < 500;
 
   if (!expectedError) {
     logger.log(error);
@@ -25,4 +28,4 @@ export default {
   put: axios.put,
   delete: axios.delete,
   setJwt,
-}
+};
